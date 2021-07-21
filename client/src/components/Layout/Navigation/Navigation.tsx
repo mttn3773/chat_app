c550a33fa2fa32350633f456e56f9a6892e2fe38
mttn373 <mttn3773@gmail.com>
2021-07-21 11:24:55 +0400

    conditional user links

--- client/src/components/Layout/Navigation/Navigation.tsx
+++ client/src/components/Layout/Navigation/Navigation.tsx
@@ -1,23 +1,29 @@
 import React from "react";
+import { useContext } from "react";
 import { config } from "../../../config";
+import { DataContext } from "../../../store/GlobalState";
 import { request } from "../../../utils/request";
 import "./Navigation.scss";
 interface NavigationProps {}
 
 export const Navigation: React.FC<NavigationProps> = ({}) => {
+  const { state } = useContext(DataContext);
   const handleLogout = async () => {
     await request({ url: config.endpoints.logout, method: "POST" });
     window.location.reload();
   };
+  const userLinks = state.user ? (
+    <div className="user-links">
+      <a href="/profile"> Profile </a>
+      <a onClick={handleLogout}> Logout </a>
+    </div>
+  ) : null;
   return (
     <nav>
       <a className="logo" href="/chat">
         CHAT
       </a>
-      <div className="user-links">
-        <a href="/profile"> Profile </a>
-        <a onClick={handleLogout}> Logout </a>
-      </div>
+      {userLinks}
     </nav>
   );
 };
