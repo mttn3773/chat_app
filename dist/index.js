e7710a85028edf544dfec058902b90b9847db586
mttn373 <mttn3773@gmail.com>
2021-07-21 09:26:48 +0400

    added catch block

--- dist/index.js
+++ dist/index.js
@@ -35,32 +35,37 @@ const app = express_1.default();
 const server = http_1.createServer(app);
 const io = new socket_io_1.Server(server);
 (() => __awaiter(void 0, void 0, void 0, function* () {
-    yield typeorm_1.createConnection(config_1.default.ormConfig);
-    app.use(body_parser_1.json());
-    app.use(body_parser_1.urlencoded({ extended: false }));
-    app.use(cors_1.default());
-    app.use(cookie_parser_1.default(index_2.SESSION_SECRET));
-    const RedisStore = connect_redis_1.default(express_session_1.default);
-    const redisClient = redis_1.createClient();
-    app.use(express_session_1.default(Object.assign(Object.assign({}, config_1.default.session), { store: new RedisStore({ client: redisClient, disableTouch: true }) })));
-    app.use(passport_1.default.initialize());
-    app.use(passport_1.default.session());
-    io.on("connection", (socket) => {
-        console.log(`User connected`);
-        index_1.socketLogic(socket, io);
-    });
-    passport_1.default.use(passport_2.localStrategy);
-    app.use("/api/users", user_routes_1.default);
-    app.use("/api/auth", auth_routes_1.default);
-    if (process.env.NODE_ENV === "production") {
-        app.use("/", express_1.default.static(path_1.default.join(__dirname, "..", ".", "client", "build")));
-        app.get("*", (_req, res) => {
-            res.sendFile(path_1.default.resolve(__dirname, "..", ".", "client", "build", "index.html"));
+    try {
+        yield typeorm_1.createConnection(config_1.default.ormConfig);
+        app.use(body_parser_1.json());
+        app.use(body_parser_1.urlencoded({ extended: false }));
+        app.use(cors_1.default());
+        app.use(cookie_parser_1.default(index_2.SESSION_SECRET));
+        const RedisStore = connect_redis_1.default(express_session_1.default);
+        const redisClient = redis_1.createClient();
+        app.use(express_session_1.default(Object.assign(Object.assign({}, config_1.default.session), { store: new RedisStore({ client: redisClient, disableTouch: true }) })));
+        app.use(passport_1.default.initialize());
+        app.use(passport_1.default.session());
+        io.on("connection", (socket) => {
+            console.log(`User connected`);
+            index_1.socketLogic(socket, io);
+        });
+        passport_1.default.use(passport_2.localStrategy);
+        app.use("/api/users", user_routes_1.default);
+        app.use("/api/auth", auth_routes_1.default);
+        if (process.env.NODE_ENV === "production") {
+            app.use("/", express_1.default.static(path_1.default.join(__dirname, "..", ".", "client", "build")));
+            app.get("*", (_req, res) => {
+                res.sendFile(path_1.default.resolve(__dirname, "..", ".", "client", "build", "index.html"));
+            });
+        }
+        server.listen(config_1.default.server.port, () => {
+            console.log(`App is running on port ${config_1.default.server.port}`);
         });
     }
-    server.listen(config_1.default.server.port, () => {
-        console.log(`App is running on port ${config_1.default.server.port}`);
-    });
+    catch (e) {
+        console.log(e);
+    }
 }))();
 exports.default = app;
 //# sourceMappingURL=index.js.map
